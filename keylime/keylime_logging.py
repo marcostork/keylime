1be634bd034df3c7e8a0d33aaaaa157646e6b5ae
Marcos Tork <marcos.tork@gmail.com>
2024-12-06 12:03:57 -0300

    Adding extract_update to retrieve logging configuration from component configuration files
    Signed-off-by: Marcos Tork <marcos.tork@gmail.com>

--- keylime/keylime_logging.py
+++ keylime/keylime_logging.py
@@ -1,5 +1,6 @@
 import contextvars
-import logging
+import logging, ast
+from configparser import RawConfigParser
 from logging import Logger
 from logging import config as logging_config
 from typing import TYPE_CHECKING, Any, Callable, Dict
@@ -57,6 +58,50 @@ def annotate_logger(logger: Logger) -> None:
 
     for handler in logger.handlers:
         handler.addFilter(request_id_filter)
+
+def parse_field(key, value):
+    """Handle special cases for specific logging fields."""
+    if key == 'args':
+        return ast.literal_eval(value)  # Safely parse tuple or list
+    elif key in ['handlers', 'filters']:
+        return [item.strip() for item in value.split(',')]  # Convert to list
+    elif key in ['propagate', 'disable_existing_loggers']:
+        return value.lower() in ['true', '1']  # Convert to boolean
+    elif key == 'level':
+        return getattr(logging, value.upper(), value)  # Map to logging level constant
+    else:
+        return value  # Default case for other fields
+
+
+def extract_update(raw_config: RawConfigParser):
+    """Reads the component configuration and extracts any logging configuration available.
+    Returns a dictionary that can be used to update logging configuration"""
+    logging_conf_update = {}
+
+    for section in raw_config.sections():
+        if section.startswith('logging'):
+            logging_conf_update = {key: value for key, value in raw_config[section].items()}
+        elif section.startswith('formatter_'):
+            formatter_name = section.split('_', 1)[1]
+            logging_conf_update.setdefault('formatters', {})[formatter_name] = {
+                key: parse_field(value) for key, value in raw_config.items(section)
+            }
+        elif section.startswith('logger_'):
+            logger_name = section.split('_', 1)[1]
+            logging_conf_update.setdefault('loggers', {})[logger_name] = {
+                key: parse_field(value) for key, value in raw_config.items(section)
+            }
+        elif section.startswith('handlers_'):
+            handler_name = section.split('_', 1)[1]
+            logging_conf_update.setdefault('handlers', {})[handler_name] = {
+                key: parse_field(value) for key, value in raw_config.items(section)
+            }
+    
+    # Check if mandatory 'version' is present, otherwise retrieve it from the original configuration
+    if 'version' not in logging_conf_update.keys():
+        logging_conf_update['version'] = ast.literal_eval(
+            {key: value for key, value in config.get_config("logging")['logging'].items()}['version']
+        )
 
 
 def init_logging(loggername: str) -> Logger:
